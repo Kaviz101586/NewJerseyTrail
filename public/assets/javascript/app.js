647d4ac22c713513dbae9b39fd9e693e9b37cd72
Emalley <EMalley998@gmail.com>
2019-03-06 10:43:41 -0800

    Lore Page added, and working order for Javascript

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,17 +1,23 @@
-$(document).ready(function() {
+$(document).ready(function () {
     function initializePage() {
         $("#progress-page").hide();
+        $("#info-page").hide();
     };
     initializePage();
-$("#start-button").on("click", function() {
-    $("#welcome-page").hide();
-    $("#progress-page").show();
-    loadPageDisplay();
-});
+    $("#start-button").on("click", function () {
+        $("#welcome-page").hide();
+        $("#info-page").show();
+    });
+    
+    $("#begin-button").on("click", function(){
+        $("#info-page").hide();
+        $("#progress-page").show();
+    });
 
-function loadPageDisplay() {
-    $("#img-div").html("<img src='assets/images/driving-cat.gif'></img>");
-}
+    function loadPageDisplay() {
+        $("#img-div").html("<img src='assets/images/driving-cat.gif'></img>");
+    }
+    loadPageDisplay();
 
 })
 
