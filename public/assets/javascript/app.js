27f08d933f696170cb88837e6b25c00b094d0e52
Chesterfield <boyce.davidw@gmail.com>
2019-03-06 20:33:57 -0500

    updated package.json and travel function

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -19,9 +19,41 @@ $(document).ready(function () {
     $("#startGame-button").on("click", function(){
         $("#storeFront").hide();
         $("#gamePlay").show();
+        var intervalId = setInterval(travel, 1000);
     });
     
     initializePage();
+
+
+
+var timer = 5;
+
+function travel() {
+    timer --;
+
+    distance - 5;
+    food - 1;
+    water - 1;
+    gas - 2;
+
+    $("#food-remaining").text("Food: ") + food;
+    $("#water-remaining").text("Water: ") + water;
+    $("#gas-remaining").text("Gas: ") + gas;
+    $("#distance-remaining").text("Distance Remaining: ") + distance;
+
+    if (timer == 0) {
+        eventRandom();
+    }
+};
+
+function eventRandom() {
+    // given a certain probability, an event will be called.
+    // if the event is not called, timer resets to 5 (or whatever interval is decided)
+    // if the event is called, 
+};
+
+function displayEvent() {
+    $("#gamePlay").show().append();   
+};
+
 })
-
-
