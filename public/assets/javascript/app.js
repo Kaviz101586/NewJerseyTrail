089d6fd29d433ccccd359c058302f19c2c83565b
Emalley <EMalley998@gmail.com>
2019-03-08 14:30:36 -0800

    massive CSS update

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,24 +1,67 @@
 $(document).ready(function () {
+// carosuel for front page
+$('.carousel').carousel({
+    interval: 3000
+  })
+
+
+// Game Logic
     function initializePage() {
-        $("#progress-page").hide();
+        $("#randomEvent").hide();
         $("#info-page").hide();
+        $("#storeFront").hide();
+        $("#gamePlay").hide();
     };
-    initializePage();
+
     $("#start-button").on("click", function () {
         $("#welcome-page").hide();
         $("#info-page").show();
     });
     
-    $("#begin-button").on("click", function(){
+    // $("#store-button").on("click", function(){
+    //     $("#info-page").hide();
+    //     $("#storeFront").show();
+    // });
+    
+    $("#startGame-button").on("click", function(){
         $("#info-page").hide();
-        $("#progress-page").show();
+        $("#storeFront").hide();
+        $("#gamePlay").show();
+        var intervalId = setInterval(travel, 1000);
     });
+    
+    initializePage();
 
-    function loadPageDisplay() {
-        $("#img-div").html("<img src='assets/images/driving-cat.gif'></img>");
+
+
+var timer = 5;
+
+function travel() {
+    timer --;
+
+    // distance - 5;
+    food -= 1;
+    water -= 1;
+    gas -= 2;
+
+    $("#food-remaining").text("Food: ") + food;
+    $("#water-remaining").text("Water: ") + water;
+    $("#gas-remaining").text("Gas: ") + gas;
+    $("#distance-remaining").text("Distance Remaining: ") + distance;
+
+    if (timer == 0) {
+        eventRandom();
     }
-    loadPageDisplay();
+};
+
+function eventRandom() {
+    // given a certain probability, an event will be called.
+    // if the event is not called, timer resets to 5 (or whatever interval is decided)
+    // if the event is called, 
+};
+
+function displayEvent() {
+    $("#gamePlay").show().append();   
+};
 
 })
-
-
